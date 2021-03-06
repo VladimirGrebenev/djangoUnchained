128477cdfba83f9c7416c02c2e9f7da59320cd27
VladimirGrebenev <grebenev-81@mail.ru>
2021-03-06 16:46:01 +0300

    Добавлено на сайте: восстановление пароля, пагинация, добавление и редактирование статей / Обучение в itProger

--- djangoUnchained/settings.py
+++ djangoUnchained/settings.py
@@ -131,9 +131,9 @@ EMAIL_HOST_PASSWORD = 'dqhugfoxbixyzqtv'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'user'
